fe1d7febf7ac8914c9ee50ba989e5f1a6a4b1b90
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 21:00:24 -0500

    :tada: Begin

---
+++ me.ts
@@ -0,0 +1,6 @@
+export const publisher = {
+  name: "Eliaz Bobadilla",
+  username: "ultirequiem",
+  email: "eliaz.bobadilladev@gmail.com",
+  site: "https://ultirequiem.com",
+} as const;
---
+++ node_support.ts
@@ -0,0 +1,73 @@
+import { build, EntryPoint } from "https://deno.land/x/dnt@0.22.0/mod.ts";
+
+import { publisher } from "./me.ts";
+
+import type { ShimOptions } from "https://deno.land/x/dnt@0.22.0/lib/shims.ts";
+
+export async function buildPackage(
+  packageConfig: {
+    keywords: string[];
+    homepage: string;
+    repoName: string;
+    name?: string;
+    version: string;
+    license?: string;
+    description: string;
+  },
+  buildOptions: {
+    entryPoints?: EntryPoint[];
+    outDir?: string;
+    shims?: ShimOptions;
+  } = {}
+) {
+  const {
+    entryPoints = ["./mod.ts"],
+    outDir = "./node",
+    shims = { deno: true },
+  } = buildOptions;
+
+  const {
+    keywords,
+    homepage,
+    repoName,
+    name = repoName,
+    version,
+    license = "MIT",
+    description,
+  } = packageConfig;
+
+  await build({
+    entryPoints: entryPoints,
+
+    outDir,
+
+    shims,
+
+    package: {
+      name: name,
+      description: description,
+
+      version: version,
+
+      license: license,
+
+      funding: {
+        type: "patreon",
+        url: `https://www.patreon.com/${publisher.username}`,
+      },
+
+      repository: `github:${publisher.username}/${repoName}`,
+      homepage,
+
+      bugs: {
+        url: `https://github.com/${publisher.username}/${repoName}/issues`,
+        email: publisher.email,
+      },
+
+      keywords,
+    },
+  });
+
+  Deno.copyFileSync("license", "node/LICENSE");
+  Deno.copyFileSync("readme.md", "node/README.md");
+}
